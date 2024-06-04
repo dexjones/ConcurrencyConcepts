bc07d00fcd7c049f350ad451661f90a446b480cb
Dexter <dexter.t.jones@outlook.com>
2024-06-03 19:25:10 -0600

    Add project files.

---
+++ ConcurrencyConcepts/Concurrency.cpp
@@ -0,0 +1,13 @@
+/*
+Author: Dexter Jones
+
+- Program that exhibits concurrency concepts
+- Creates two threads that act as counters
+- One thread counts up to 20
+- Once Thread one reaches 20, second thread counts down to 0
+- Provide detailed analysis of appropriate concepts that impact application, address:
+	- Performance issues with concurrency
+	- Vulnerabilities exhibited with use of strings
+	- Security of the data types exhibited
+*/
+#include <iostream>
